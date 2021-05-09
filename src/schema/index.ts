49a2ee6dfe4e7f5fa4637612ad57929d54f636ff
LucasGobatto <lucas.bisaio@ive.com>
2021-05-09 13:33:13 -0300

    feat: add lint command and apply code pattern

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,13 +1,13 @@
 import { gql } from "apollo-server-core";
 
 export const resolvers = {
-	Query: {
-		hello: () => 'Hello World!',
-	}
+  Query: {
+    hello: () => "Hello World!",
+  },
 };
 
 export const typeDefs = gql`
-	type Query { 
-		hello: String!
-	}
+  type Query {
+    hello: String!
+  }
 `;
