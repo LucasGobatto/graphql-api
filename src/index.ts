0a9fc2cdb599f2b1eea849d60a5f8859c962181c
LucasGobatto <lucas.bisaio@ive.com>
2021-05-09 23:38:08 -0300

    feat: condig docker and DB

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,5 @@
-import 'reflect-metadata';
-import { ApolloServer } from "apollo-server-express";
-import { createServer } from "http";
-import express from "express";
-import dotenv from 'dotenv';
+import "reflect-metadata";
 
-import { resolvers, typeDefs } from "./schema";
+import { bootstrap } from "./app";
 
-const path = process.env.TEST === 'OK' ? '.env' : '.test.env'
-dotenv.config({ path });
-
-console.log(process.env)
-const server = new ApolloServer({ resolvers, typeDefs });
-
-const app = express();
-
-server.applyMiddleware({ app, path: "/graphql" });
-
-const PORT = process.env.PORT ?? 4000;
-const httpServer = createServer(app);
-
-httpServer.listen(PORT, () => {
-  console.log(`Listen at http://localhost:${PORT}/graphql`);
-});
+bootstrap();
