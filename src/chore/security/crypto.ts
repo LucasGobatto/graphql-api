85201d72e6d9804ac1e4f54fde58636c49548cca
LucasGobatto <lucas.bisaio@ive.com>
2021-05-10 21:17:12 -0300

    fix: type of param

--- src/chore/security/crypto.ts
+++ src/chore/security/crypto.ts
@@ -12,7 +12,7 @@ function hash(value: string): Promise<string> {
   });
 }
 
-function verify(value: string, hash: any): Promise<boolean> {
+function verify(value: string, hash: string): Promise<boolean> {
   return new Promise((resolve, reject) => {
     const [salt, key] = hash.split(':');
     crypto.scrypt(value, salt, 64, (err, derivedKey) => {
