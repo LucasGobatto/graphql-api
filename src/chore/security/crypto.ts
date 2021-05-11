e3caa63d824c15d5ec0ae277226f92fa93ec77ab
LucasGobatto <lucas.bisaio@ive.com>
2021-05-10 21:15:53 -0300

    feat: add a verify hashed password method

--- src/chore/security/crypto.ts
+++ src/chore/security/crypto.ts
@@ -12,6 +12,17 @@ function hash(value: string): Promise<string> {
   });
 }
 
+function verify(value: string, hash: any): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    const [salt, key] = hash.split(':');
+    crypto.scrypt(value, salt, 64, (err, derivedKey) => {
+      if (err) reject(err);
+      resolve(key == derivedKey.toString('hex'));
+    });
+  });
+}
+
 export const CryptoService = {
   hash,
+  verify,
 };
