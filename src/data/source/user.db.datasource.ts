f3816e921d34994a8346c34846ce5e64ea0c167c
LucasGobatto <lucas.bisaio@live.com>
2022-04-23 12:08:05 -0300

    add typedi and refact folder structure

---
+++ src/data/source/user.db.datasource.ts
@@ -0,0 +1,22 @@
+import { Service } from "typedi"; 
+import { Repository } from "typeorm";
+import { InjectRepository } from "typeorm-typedi-extensions";
+import { UserEntity } from "../../data/db/entity/user.entity";
+import { PaginatedInputType } from "../../domain/model";
+
+@Service()
+export class UserDbDataSource {
+  constructor(@InjectRepository(UserEntity) private readonly repository: Repository<UserEntity>) {}
+  
+  findById(id: string) {
+    return this.repository.findOne(id);
+  }
+
+  findOneByEmail(email: string) {
+    return this.repository.findOne({ where: { email }});
+  }
+
+  findAndCount({ take, skip }: PaginatedInputType) {
+    return this.repository.findAndCount({ order: { id: 'ASC' }, take, skip })
+  }
+}
