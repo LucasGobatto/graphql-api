147dc2123114251721b2e3e4b16bf0e4bc09903d
LucasGobatto <lucas.bisaio@live.com>
2022-04-25 20:36:07 -0300

    format error

--- src/domain/login.use-case.ts
+++ src/domain/login.use-case.ts
@@ -37,13 +37,13 @@ export class LoginUseCase {
     const user = await this.userDbDataSource.findOneByEmail(email);
 
     if (!user) {
-      throw new NotFoundError(undefined, "User not found");
+      throw new NotFoundError(undefined, "Invalid credentials");
     }
 
     const isValid = await CryptoService.verify(password, user.password);
 
     if (!isValid) {
-      throw new AuthError("Invalid password");
+      throw new AuthError("Invalid credentials");
     }
 
     delete user.password;
