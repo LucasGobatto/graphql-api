ac8dbca7123fe416b915fff1c9bed7d4871d3700
LucasGobatto <lucas.bisaio@live.com>
2022-05-17 00:20:21 -0300

    wip

--- src/data/db/config/database.config.ts
+++ src/data/db/config/database.config.ts
@@ -1,4 +1,5 @@
 import { logger } from "@core/logger";
+import * as path from "path";
 import { Connection, createConnection } from "typeorm";
 
 interface DatabaseParams {
@@ -19,8 +20,12 @@ export class Database {
       username: param.username,
       password: param.password,
       database: param.database,
-      entities: [__dirname + "/../entity/*.ts"],
-      synchronize: true,
+      entities: [path.join(__dirname, "..", "entity", "*.ts")],
+      migrations: [path.join(__dirname, "..", "migrate", "*", "**.migrate.ts")],
+      cli: {
+        migrationsDir: path.join(__dirname, "..", "migrate"),
+      },
+      synchronize: false,
     });
   }
 }
